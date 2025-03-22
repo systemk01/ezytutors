bb1c67f3ca5f23ca71226f7e5f93d721679a830a
systemk01 <systemk1@icloud.com>
2025-03-22 02:28:15 +0100

    defined course_routes in App to create new courses, to get courses for tutor and to get course details, created routes, wrote handlers

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -1,6 +1,15 @@
 use super::handlers::*;
-use actix_web::web;
+use actix_web::web::{self};
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
+}
+
+pub fn course_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/courses") //creates an new route scope under which all apis are added
+            .route("/", web::post().to(new_course))
+            .route("/{tutor_id}", web::get().to(get_courses_for_tutor))
+            .route("/{tutor_id}/{course_id}", web::get().to(get_course_detail)),
+    );
 }
