865c0718a78d4306c773b5d007c434d3603d65c6
systemk01 <systemk1@icloud.com>
2025-03-25 16:35:20 +0100

    finished iter2 with routes, handlers, state, main and tests

--- tutor-db/src/bin/iter2.rs
+++ tutor-db/src/bin/iter2.rs
@@ -1,6 +1,40 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use std::env;
 use std::io;
+use std::sync::Mutex;
+
+#[path = "../iter2/handlers.rs"]
+mod handlers;
+#[path = "../iter2/models.rs"]
+mod models;
+#[path = "../iter2/routes.rs"]
+mod routes;
+#[path = "../iter2/state.rs"]
+mod state;
+
+use routes::*;
+use state::AppState;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-Ok(())
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    //Construct AppState
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good, you already asked me".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+    //Construct App and configure routes
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+    //start HTTP server
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
